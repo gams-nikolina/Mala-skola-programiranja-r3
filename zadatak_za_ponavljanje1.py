5dcdc203d4f1c37ad8e796ded5e1b58c4d10153b
gams-nikolina <80062473+gams-nikolina@users.noreply.github.com>
2023-01-03 15:01:32 +0100

    Add files via upload

---
+++ zadatak_za_ponavljanje1.py
@@ -0,0 +1,9 @@
+#1. Zadatak za ponavljanje: Napiši program koji unosi osobno ime te ispisuje poruku je li učitano ime muško
+#ili žensko. Ako ime zavrsava slovom ’a’ ispisat će se poruka ’Žensko ime’, a u svim ostalim
+#slučajevima ’Muško ime’. Iznimke cemo zanemariti.
+
+ime=input('Upiši ime:  ')
+if ime[-1]=='a':
+    print('Žensko ime')
+else:
+    print('Muško ime')
